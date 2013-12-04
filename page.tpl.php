e1f84d779cb355c803741f8796abad0b8a3760cc
Jurgen Verhasselt <sjugge@heretiksambrosia.net>
2013-12-04 21:32:56 +0100

    header love, slogan stuffs etc

--- page.tpl.php
+++ page.tpl.php
@@ -13,6 +13,7 @@
   <div id="header" class='reverse'><div class='limiter clear-block'>
     <div class='logo'><img src="/sites/all/themes/custom/weldir/weldir-logo.png"></div>
     <?php if ($site_name): ?><div class='site-name'><?php print $site_name ?></div><?php endif; ?>
+    <?php if ($site_slogan): ?><div class='site-slogan'>| <?php print $site_slogan ?></div><?php endif; ?>
     <?php if ($search_box) print $search_box ?>
   </div></div><!-- /header -->
 
