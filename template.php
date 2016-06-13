a48e5e0856934fbdca22dcb4b0e79acc3364cd0a
timdiels1 <tim.diels@wunderkraut.com>
2016-06-13 17:05:19 +0200

    Add the user's roles as body classes

--- template.php
+++ template.php
@@ -5,4 +5,12 @@
  */
 function weldir_preprocess_page(&$vars) {
    $vars['logo'] = theme_get_setting('logo');
+
+   // Get & print the roles of current user as body classes
+   if ($vars['user']) {
+      foreach ($vars['user']->roles as $key => $role) {
+         $role_class = "role-" . strtolower(str_replace(' ', '-', $role));
+         $vars['body_classes'] .= ' ' . $role_class;
+      }
+   }
 }
